7313872935fab8faa9a6c3e451f008c192a12a0c
Prabhakaran Marimuthu <prabhakaran.m@ionixxtech.com>
2025-09-24 16:08:02 +0530

    Initial Commit of Maven Framework

---
+++ src/main/java/web_automation/Base/BaseTest.java
@@ -0,0 +1,26 @@
+package web_automation.Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
